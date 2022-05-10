429b77166946fe53d87e808ec3c5c994a50549c0
Jake Hopkins <j.hopkins7923@student.leedbeckett.ac.uk>
2022-05-10 22:51:12 +0100

    Cleaned js

--- html/listener.js
+++ html/listener.js
@@ -1,41 +1,32 @@
 var warningVisible, oldWarning;
 
 $(document).ready(function() {
-	var $container = $("#container");
-	var $boxHealth = $("#box");
-	var $boxFood = $("#boxFood");
-	var $boxWater = $("#boxWater");
-	var $boxFuel = $("#boxFuel");
-	var $boxNos = $("#boxNos");
 	oldWarning = "none";
 	warningVisible = "none";
 
     $("#hud").hide();
 	$("#vehicle").hide();
 	$('#notification').hide();
-	$container.hide();
+	$("#container").hide();
 	$("#change").hide();
 
-	var alert = new Howl({
-		src: ['alert.wav'],
-		volume: 0.1
-	  });
+	var alert = new Howl({src: ['alert.wav'], volume: 0.1});
 
 	window.addEventListener('message', function(event){
 		var item = event.data;
 		switch (item.type) {
 			case "Visible":
 				if (item.showHUD === true) {
-					$container.show();
+					$("#container").show();
 				} else{
-					$container.hide();
+					$("#container").hide();
 				}
 			break;
 			case "CashVisible":
 				if (item.showCash === true) {
 					$("#money").fadeIn(500);
 				} else{
-					  $("#money").fadeOut(500);
+					$("#money").fadeOut(500);
 				}
 			break;
             case "VehDataVisible":
@@ -57,20 +48,18 @@ $(document).ready(function() {
 				notify(item.title, item.content, item.delay);
 			break;
 			case "PlayerInfo":
-				$boxHealth.css("width", (item.heal-100)+"%");
-				$boxFood.css("width", (item.food)+"%");
-				$boxWater.css("width", (item.water)+"%");
+				$("#boxHealth").css("width", (item.heal-100)+"%");
+				$("#boxFood").css("width", (item.food)+"%");
+				$("#boxWater").css("width", (item.water)+"%");
 				oldCash = $('#cash').text();
 				$('#cash').text(item.cash).trigger("change");
 			break;
 			case "VehicleInfo":
-				$boxFuel.css("width", (item.fuel)+"%");
-				$boxNos.css("width", (item.nos/20)+"%");
+				$("#boxFuel").css("width", (item.fuel)+"%");
+				$("#boxNos").css("width", (item.nos/20)+"%");
 				Vehiclehealth(item.health);
 				if(warningVisible != oldWarning){
-					if(warningVisible != "none") {
-						alert.play();
-					}
+					if(warningVisible != "none") {alert.play();}
 				}
 			break;
             default:
@@ -84,17 +73,19 @@ $(document).ready(function() {
 			var numOld = parseInt(oldCash.replace(',', '').replace('$', ''));
 			var numNew = parseInt($('#cash').text().replace(',', '').replace('$', ''));
 			var diff = numNew - numOld;
-			if(diff > 0){
-				$("#change").css("color", "green");
-				$("#change").text("+"+diff);
-			} else {
-				$("#change").css("color", "red");
-				$("#change").text(diff);
+			if(!isNaN(diff)){
+				if(diff > 0){
+					$("#change").css("color", "green");
+					$("#change").text("+"+diff);
+				} else {
+					$("#change").css("color", "red");
+					$("#change").text(diff);
+				}
+				$("#change").show();
+				setTimeout(() => {  $("#change").hide(); }, 4000);
 			}
-			$("#change").show();
-			setTimeout(() => {  $("#change").hide(); }, 4000);
 		}
-	  });
+	});
 });
 
 function Vehiclehealth(val){
